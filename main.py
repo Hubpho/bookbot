d4b292f08f7676671e294ca6b927010e15b0975a
Gitpho <gitpho@gmail.com>
2026-06-01 16:35:02 -0400

    3.1 Report | Print a Report

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from stats import get_chars_dict, get_num_words
+from stats import (
+    CharacterCount,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+    get_num_words,
+)
 
 
 def main() -> None:
@@ -6,13 +11,29 @@ def main() -> None:
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(f"Found {num_words} total words")
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
+
+
+def print_report(
+    book_path: str, num_words: int, chars_sorted_list: list[CharacterCount]
+) -> None:
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
+from typing import TypedDict
+
+
+class CharacterCount(TypedDict):
+    char: str
+    num: int
+
+
 def get_num_words(text: str) -> int:
     words = text.split()
     return len(words)
@@ -12,3 +20,15 @@ def get_chars_dict(text: str) -> dict[str, int]:
         else:
             chars[lowered] = 1
     return chars
+
+
+def sort_on(d: CharacterCount) -> int:
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict: dict[str, int]) -> list[CharacterCount]:
+    sorted_list: list[CharacterCount] = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
