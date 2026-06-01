6e09697a9d1fb1913e7006eb2eb9e122046d9dce
Gitpho <gitpho@gmail.com>
2026-06-01 16:32:21 -0400

    2.4 Data Analysis | Count Words

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 def main() -> None:
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
 
 
 def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
+
+
+def get_num_words(text: str) -> int:
+    words = text.split()
+    return len(words)
 
 
 main()
