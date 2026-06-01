c628a9c57a5af4c66b608f088623e4d50b6ade8a
Gitpho <gitpho@gmail.com>
2026-06-01 16:31:33 -0400

    2.3 Data Analysis | Read File

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def main() -> None:
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path: str) -> str:
+    with open(path) as f:
+        return f.read()
+
+
+main()
